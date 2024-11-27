4713e8a2b40e660deecedc26a97f66a0b3b06c1c
TalritsPujens <talrits.pujens@grobinasskola.lv>
2024-11-27 14:30:39 +0200

    pirmo uzdevumu nesapratu bet otrais sanaca

--- uzd_01_1.py
+++ uzd_01_1.py
@@ -1,3 +1,7 @@
 # Uzdevums: 01-1: Izveidot programmu, kas prasa lietotâjam ievadît skaitli n, tad programma aprçíina n+nn+nnn. 
-Rezultâts tiek parâdîts konsolç.
+n= int(input("ievadiet skaitli:"))
+nn=int(n,n)
+nnn=int(n,n,n)
+atbilde=(n)+(nn)+(nnn)
+print(atbilde)
 
--- uzd_01_2.py
+++ uzd_01_2.py
@@ -1,5 +1,9 @@
 # Izveidot programmu, kura prasa lietotâjam ievadît cilindra râdiusu un tâ augstumu, tiek aprçíinâts cilindra laukums un tilpums. Rezultâts tiek parâdîts konsolç.
 # tilpums = 3.14 * râdiuss * râdiuss * augstums
 # laukums = 2 * (3.14 * râdiuss * râdiuss) + augstums * (2 * 3.14 * râdiuss)
-
+radiuss=int(input("ievadiet picas rādiusu"))
+augstums=int(input("ievadiet picas biezumu"))
+tilpums=3.13*radiuss*radiuss*augstums
+laukums=2*(3.14*radiuss*radiuss)+augstums*(2*3.14*radiuss)
+print3(f"picas laukums ir {laukums} un tilpums ir {tilpums}")
 
